708da3f8783c4d5fd4a13047398928f88b6947e7
Khushi chaudhari <khushichaudhari0406@gmail.com>
2025-01-23 12:52:51 +0530

    Update p13_amstrongno.c

--- day 5 while loops/p13_amstrongno.c
+++ day 5 while loops/p13_amstrongno.c
@@ -1,3 +1,26 @@
 #include <stdio.h>
 void main()
-{ }
+{
+  int num, rev,d,res=0;
+printf("Enter a number:-");
+scanf("%d", &num);
+int org=num;
+
+while(num>0)
+{
+    
+rev=d/10;
+d=num%10;
+res=res+ (d*d*d);
+num=num/10;
+
+}
+if(res==org)
+{
+    printf("the %d is armstrong number", org);
+}
+else{
+    printf("the %d is not a armstrong number", org);
+}
+
+}
