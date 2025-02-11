4b58b6f87b7fd36ab46f68ce3fd576addbe91772
Khushi chaudhari <khushichaudhari0406@gmail.com>
2025-02-11 11:21:02 +0530

    Add files via upload

---
+++ day 10 pointer/p7_reverseosstringdynamic.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    int i;
+    char name[50];
+
+    printf("Enter your name:-");
+    scanf("%s", name);
+
+    int len= strlen(name);
+
+    char *ptr;
+    ptr= &name[0];
+
+
+    printf("The reverse of %s is:-", name);
+    for(i=(len-1);i>=0;i--)
+    {
+        printf("%c", *(ptr+i));
+    }
+
+    
+
+
+
+}
